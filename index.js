6d904c3bbd5968aab4425ef39e40358536ec3294
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 22:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemDetails={itemName:[item]};
+  var itemDetails={itemName:[item], itemPrice:};
+  cart.push(itemDetails);
  // write your code here
 }
 
